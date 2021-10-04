8bd5f24e027035e35f4552f206d27a88ca95c643
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-04 21:51:40 +0900

    [clean] ft_atoi

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 18:51:43 by louisnop          #+#    #+#             */
-/*   Updated: 2021/10/02 11:43:23 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/04 21:16:35 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,31 @@ int	ft_isspace(char c)
 {
 	if (c == ' ' || c == '\n' || c == '\t'
 		|| c == '\v' || c == '\f' || c == '\r')
-	{
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
 
 int	ft_atoi(char *str)
 {
-	int	res;
+	int	num;
 	int	sign;
 	int	i;
 
-	res = 0;
-	sign = 0;
+	num = 0;
+	sign = 1;
 	i = 0;
 	while (ft_isspace(str[i]))
 		i++;
 	while (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			sign++;
+			sign *= -1;
 		i++;
 	}
 	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
 	{
-		res = res * 10 + (str[i] - '0');
+		num = num * 10 + (str[i] - '0');
 		i++;
 	}
-	if (sign % 2 == 1)
-		return (res * -1);
-	else
-		return (res);
+	return (num * sign);
 }
