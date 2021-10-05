361f450d8d95f6632bbefe146ab58db80cf782e6
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-05 15:32:00 +0900

    [clean] generate_answer: rm malloc p_coord

--- srcs/generate_answer.c
+++ srcs/generate_answer.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 21:46:00 by louisnop          #+#    #+#             */
-/*   Updated: 2021/10/04 23:34:18 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/05 15:31:06 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,26 +72,22 @@ void	place_square_on_map(char **map, t_info *p_info)
 
 void	generate_answer(char **map, t_info *p_info)
 {
-	t_coord	*p_coord;
+	t_coord	p_coord;
 
-	p_coord = malloc(sizeof(t_coord));
-	if (!p_coord)
-		exit(EXIT_FAILURE);
-	p_coord->y = 0;
-	while (p_coord->y < p_info->map_height)
+	p_coord.y = 0;
+	while (p_coord.y < p_info->map_height)
 	{
-		p_coord->x = 0;
-		while (p_coord->x < p_info->map_width)
+		p_coord.x = 0;
+		while (p_coord.x < p_info->map_width)
 		{
-			if (is_empty_spot_on_map(map, p_coord->x, p_coord->y, p_info))
+			if (is_empty_spot_on_map(map, p_coord.x, p_coord.y, p_info))
 			{
-				check_for_square(map, p_coord, p_info);
+				check_for_square(map, &p_coord, p_info);
 			}
-			p_coord->x++;
+			p_coord.x++;
 		}
-		p_coord->y++;
+		p_coord.y++;
 	}
-	free(p_coord);
 	place_square_on_map(map, p_info);
 	put_map(map, p_info);
 }
