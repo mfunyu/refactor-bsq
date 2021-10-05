38b3a5dd8dca749a9c001d6306adcc8edd37d65c
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-05 15:09:01 +0900

    [clean] free: p_map

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 02:58:38 by louisnop          #+#    #+#             */
-/*   Updated: 2021/10/05 02:05:52 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/05 15:08:17 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	free_map(char ***map)
 {
 	long int	i;
+	char		**p_map;
 
+	p_map = *map;
 	i = 0;
-	while ((*map)[i])
+	while (p_map[i])
 	{
-		free((*map)[i]);
+		free(p_map[i]);
 		i++;
 	}
-	free(*map);
-	*map = NULL;
+	free(p_map);
+	p_map = NULL;
 }
