d00718bf643056273448a3951a47862e4eefd12b
mfunyu <mfunyu@student.42tokyo.jp>
2021-10-05 15:06:07 +0900

    [clean] init_t_info: rm malloc num

--- srcs/init_t_info.c
+++ srcs/init_t_info.c
@@ -6,7 +6,7 @@
 /*   By: mfunyu <mfunyu@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 16:24:06 by mfunyu            #+#    #+#             */
-/*   Updated: 2021/10/04 23:27:44 by mfunyu           ###   ########.fr       */
+/*   Updated: 2021/10/05 15:05:11 by mfunyu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,16 @@
 
 int	init_t_info(t_info *info, char **map, char *content, int len)
 {
-	int		i;
-	char	*num;
 	int		row_size_digits;
 
-	row_size_digits = len - 3;
-	num = malloc(sizeof(char) * (row_size_digits + 1));
-	if (!num)
-		exit(EXIT_FAILURE);
-	i = -1;
-	while (++i < row_size_digits)
-		num[i] = content[i];
-	num[i] = '\0';
-	info->map_height = ft_atoi(num);
+	info->map_height = ft_atoi(content);
 	info->map_width = ft_strlen(map[0]);
+	row_size_digits = len - 3;
 	info->empty = content[row_size_digits];
 	info->obstacle = content[row_size_digits + 1];
 	info->full = content[row_size_digits + 2];
 	info->max_sq_size = 0;
 	info->sq_x_coord = 0;
 	info->sq_y_coord = 0;
-	free(num);
 	return (SUCCESS);
 }
